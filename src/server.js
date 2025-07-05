9c170abd032c07b1c0c063f222027a40e595dd87
DavidSheltonSF <david.faria012@gmail.com>
2025-07-04 21:12:14 -0300

    feat(server.js): add /register route

--- src/server.js
+++ src/server.js
@@ -3,10 +3,21 @@ const app = express();
 const port = 3000;
 
 app.use(express.json());
+app.use()
 
 app.get("/", (req, res) => {
   res.send("Eae bro? Bora finalizar esse projeto?");
 });
+
+app.post("/register", (req, res) => {
+  const {name, birthday, profile_picture} = req.body;
+
+  res.status(200).send({
+    name,
+    birthday,
+    profile_picture
+  })
+})
 
 app.listen(port, () => {
   console.log(`Server listening on port ${port}`);
