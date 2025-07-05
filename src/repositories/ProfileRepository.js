b993cf6924aac373ec594495538ab24f58962c2a
DavidSheltonSF <david.faria012@gmail.com>
2025-07-05 18:07:45 -0300

    feat(ProfileRepository)

---
+++ src/repositories/ProfileRepository.js
@@ -0,0 +1,57 @@
+const MysqlHelper = require('./helper/MysqlHelper');
+
+class profileRepository {
+  databaseHandler = MysqlHelper.create();
+
+  async findAllprofiles() {
+    const connection = this.databaseHandler.getConnection();
+
+    const result = await connection.query('SELECT * FROM profiles');
+
+    return result[0];
+  }
+
+  async findprofileById(id) {
+    const connection = this.databaseHandler.getConnection();
+
+    const result = await connection.query('SELECT * FROM profiles WHERE id=?', [
+      id,
+    ]);
+
+    return result[0];
+  }
+
+  async add(profileData) {
+    const connection = this.databaseHandler.getConnection();
+
+    const { user_id, birthday, profile_picture } = profileData;
+
+    let profileId = undefined;
+    const [result] = await connection.query(
+      `
+      INSERT INTO profiles (user_id, birthday, profile_picture) VALUES (?, ?, ?)
+    `,
+      [user_id, birthday, profile_picture]
+    );
+
+    profileId = await result.insertId;
+
+    const response = await connection.query(
+      'SELECT * FROM profiles WHERE id=?',
+      [profileId]
+    );
+
+    if (response[0].length === 0) {
+      return null;
+    }
+
+    const [foundprofile] = response[0];
+
+    return {
+      id: profileId,
+      ...foundprofile,
+    };
+  }
+}
+
+module.exports = profileRepository;
---
+++ src/repositories/ProfileRepository.test.js
@@ -0,0 +1,141 @@
+const ProfileRepository = require('./ProfileRepository');
+const MysqlHelper = require('./helper/MysqlHelper');
+
+describe('Testing profileRepository', () => {
+  async function makeConnectionSUT() {
+    const mysqlHelper = MysqlHelper.create();
+    mysqlHelper.connect();
+    const connection = mysqlHelper.getConnection();
+
+    if (!connection) {
+      throw new Error('Connection is null');
+    }
+
+    return connection;
+  }
+
+  beforeEach(async () => {
+    try {
+      const connection = await makeConnectionSUT();
+      await connection.execute('TRUNCATE TABLE profiles');
+    } catch (err) {
+      console.log(err);
+    }
+  });
+
+  afterAll(async () => {
+    const connection = await makeConnectionSUT();
+    connection.end();
+  });
+
+  test('Should add a new profile in the database', async () => {
+    const connection = await makeConnectionSUT();
+    const profileRepository = new ProfileRepository();
+
+    const fakeprofile = {
+      user_id: 1,
+      birthday: new Date('2002-02-26'),
+      profile_picture: 'https://path.com/daafsfda',
+    };
+
+    const createdprofile = await profileRepository.add(fakeprofile);
+
+    const result = await connection.query('SELECT * FROM profiles WHERE id=?', [
+      createdprofile.id,
+    ]);
+
+    const [foundprofile] = result[0];
+
+    expect(createdprofile.id).toBeTruthy();
+    expect(foundprofile.profilename).toBe(fakeprofile.profilename);
+    expect(foundprofile.password).toBe(fakeprofile.password);
+    expect(foundprofile.email).toBe(fakeprofile.email);
+    expect(foundprofile.phone).toBe(fakeprofile.phone);
+  });
+
+  test('Should find all profiles in the database', async () => {
+    const connection = await makeConnectionSUT();
+    const profileRepository = new ProfileRepository();
+
+    const fakeUsers = [
+      ['TesterJARIA', 'test123', 'test@bugmail', '2325232'],
+      ['TesterSUPREM', 'test123', 'test111@bugmail', '2325855241'],
+    ];
+
+    const fakeprofiles = [
+      [1, new Date('2005-02-4'), 'https://path.com/115fsda'],
+      [2, new Date('2005-12-4'), 'https://path.com/dafsfda'],
+    ];
+
+    // Add fake users first
+    await connection.query(
+      `
+      INSERT INTO users (username, password, email, phone) VALUES ?
+    `,
+      [fakeUsers]
+    );
+
+    // Add user profiles
+    await connection.query(
+      `
+      INSERT INTO profiles (user_id, birthday, profile_picture) VALUES ?
+    `,
+      [fakeprofiles]
+    );
+
+    const result = await profileRepository.findAllprofiles();
+
+    expect(result.length).toBe(fakeprofiles.length);
+  });
+
+  test('Should find a profile by id', async () => {
+    const connection = await makeConnectionSUT();
+    const profileRepository = new ProfileRepository();
+
+    const fakeUsers = [
+      ['TesterJARIA', 'test123', 'test@bugmail', '2325232'],
+      ['TesterSUPREM', 'test123', 'test111@bugmail', '2325855241'],
+    ];
+
+    const fakeprofiles = [
+      [1, new Date('2005-02-4'), 'https://path.com/115fsda'],
+      [2, new Date('2005-12-4'), 'https://path.com/dafsfda'],
+    ];
+
+    // Add fake users first
+    await connection.query(
+      `
+      INSERT INTO users (username, password, email, phone) VALUES ?
+    `,
+      [fakeUsers]
+    );
+
+    // Add user profiles
+    const [results] = await connection.query(
+      `
+      INSERT INTO profiles (user_id, birthday, profile_picture) VALUES ?
+    `,
+      [fakeprofiles]
+    );
+
+    const firstprofileId = results.insertId;
+    const secondprofileId = results.insertId + 1;
+
+    const [foundFirstprofile] = await profileRepository.findprofileById(
+      firstprofileId
+    );
+    const [foundSecondprofile] = await profileRepository.findprofileById(
+      secondprofileId
+    );
+
+    expect(foundFirstprofile.user_id).toBe(fakeprofiles[0][0]);
+    expect(foundFirstprofile.birthday.getTime()).toBe(
+      fakeprofiles[0][1].getTime()
+    );
+
+    expect(foundSecondprofile.user_id).toBe(fakeprofiles[1][0]);
+    expect(foundSecondprofile.birthday.getTime()).toBe(
+      fakeprofiles[1][1].getTime()
+    );
+  });
+});
