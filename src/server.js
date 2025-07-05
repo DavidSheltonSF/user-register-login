0031249afcd306bfb7d3a9be6f4f040c6e160cad
DavidSheltonSF <david.faria012@gmail.com>
2025-07-05 11:49:54 -0300

    feat(server.js): use UserRepository in /retister route

--- src/server.js
+++ src/server.js
@@ -1,21 +1,31 @@
 const express = require('express')
+const UserRepository = require('./repositories/UserRepository')
 const app = express();
 const port = 3000;
 
 app.use(express.json());
-app.use()
 
 app.get("/", (req, res) => {
   res.send("Eae bro? Bora finalizar esse projeto?");
 });
 
-app.post("/register", (req, res) => {
-  const {name, birthday, profile_picture} = req.body;
+app.post("/register", async (req, res) => {
+  const { username, password, email, phone } = req.body;
+  const userRepository = new UserRepository();
+  const userId = await userRepository.add({
+    username,
+    password,
+    email,
+    phone
+  })
 
+  console.log(userId)
   res.status(200).send({
-    name,
-    birthday,
-    profile_picture
+    userId,
+    username,
+    password,
+    email,
+    phone
   })
 })
 
