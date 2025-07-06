eef81ce8fcb21442d246ae9110e1e0c1673e60de
DavidSheltonSF <david.faria012@gmail.com>
2025-07-06 14:42:55 -0300

    refactor(register): add error handling

--- src/routes/register.js
+++ src/routes/register.js
@@ -1,12 +1,28 @@
 const UserController = require('../controllers/UserController');
 
 async function register(req, res) {
-  const userController = new UserController();
-  const response = await userController.create(req);
+  try {
+    const userController = new UserController();
+    const response = await userController.create(req);
 
-  res.status(response.status).send({
-    body: response.body,
-  });
+    if (response.error){
+      res.status(response.status).send({
+        status: response.status,
+        error: response.error,
+        message: response.message
+      })
+    }
+
+    res.status(response.status).send({
+      body: response.body,
+    });
+  } catch (error) {
+    res.status(500).send({
+      status: 500,
+      error: 'ServerError',
+      message: 'Something went wrong inside the server'
+    })
+  }
 }
 
 module.exports = register
