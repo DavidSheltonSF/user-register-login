57b5f172d6fbf85a5a98a9e02d8d3174854ac4b0
DavidSheltonSF <david.faria012@gmail.com>
2025-07-07 09:57:19 -0300

    test(ProfileRepository): add more expect functions to findAllProfiles method test

--- src/repositories/ProfileRepository.test.js
+++ src/repositories/ProfileRepository.test.js
@@ -82,7 +82,18 @@ describe('Testing profileRepository', () => {
     const result = await profileRepository.findAllprofiles();
 
     expect(result.length).toBe(fakeProfiles.length);
-    //expect(result[0].user_id).toBe(fakeProfiles[0].user_id)
+
+    expect(result[0].user_id).toBe(fakeProfiles[0].user_id);
+    expect(result[0].birthday.getTime()).toBe(
+      fakeProfiles[0].birthday.getTime()
+    );
+    expect(result[0].profile_picture).toBe(fakeProfiles[0].profile_picture);
+
+    expect(result[1].user_id).toBe(fakeProfiles[1].user_id);
+    expect(result[1].birthday.getTime()).toBe(
+      fakeProfiles[1].birthday.getTime()
+    );
+    expect(result[1].profile_picture).toBe(fakeProfiles[1].profile_picture);
   });
 
   test('Should find a profile by id', async () => {
