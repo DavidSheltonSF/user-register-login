757bc61b3b2a10a54c1743335fdf333eac07a530
DavidSheltonSF <david.faria012@gmail.com>
2025-07-08 16:13:10 -0300

    refactor(backend/UserService): add userRepository and profileRepository attributes

--- backend/src/services/UserService.js
+++ backend/src/services/UserService.js
@@ -5,30 +5,29 @@ const NotOwnerError = require('./errors/NotOwnerError');
 const BcryptHelper = require('./helpers/BcryptHelper');
 
 class UserService {
+  userRepository = new UserRepository();
+  profileRepository = new ProfileRepository();
   async create(userData) {
     const { username, password, email, phone, birthday, profile_picture } =
       userData;
 
     const hashedPassword = await BcryptHelper.hashPassword(password);
 
-    const userRepository = new UserRepository();
-    const profileRepository = new ProfileRepository();
-
-    const existingUser = await userRepository.findUserByEmail(email);
+    const existingUser = await this.userRepository.findUserByEmail(email);
 
     if (existingUser && existingUser.email === email) {
       throw new DuplicatedEmailError(email);
     }
-    
+
     try {
-      const registredUser = await userRepository.add({
+      const registredUser = await this.userRepository.add({
         username,
         password: hashedPassword,
         email,
         phone,
       });
 
-      const createdProfile = await profileRepository.add({
+      const createdProfile = await this.profileRepository.add({
         user_id: registredUser.id,
         birthday,
         profile_picture,
@@ -48,16 +47,15 @@ class UserService {
       throw new NotOwnerError();
     }
 
-    const userRepository = new UserRepository();
-    const profileRepository = new ProfileRepository();
-
-    const user = await userRepository.findUserById(id);
+    const user = await this.userRepository.findUserById(id);
 
     if (!user) {
       return null;
     }
 
-    const userProfile = await profileRepository.findprofileByUserId(user.id);
+    const userProfile = await this.profileRepository.findprofileByUserId(
+      user.id
+    );
     return {
       ...user,
       profile: userProfile,
@@ -65,16 +63,15 @@ class UserService {
   }
 
   async findByEmail(email) {
-    const userRepository = new UserRepository();
-    const profileRepository = new ProfileRepository();
-
-    const user = await userRepository.findUserByEmail(email);
+    const user = await this.userRepository.findUserByEmail(email);
 
     if (!user) {
       return null;
     }
 
-    const userProfile = await profileRepository.findprofileByUserId(user.id);
+    const userProfile = await this.profileRepository.findprofileByUserId(
+      user.id
+    );
 
     return {
       ...user,
