5237a850df73146ab0a26fa5f4facc10e4560f39
DavidSheltonSF <david.faria012@gmail.com>
2025-07-11 20:25:37 -0300

    refactor(register): simplify response handling in register function

--- backend/src/routes/register.js
+++ backend/src/routes/register.js
@@ -5,24 +5,14 @@ async function register(req, res) {
     const userController = new UserController();
     const response = await userController.create(req);
 
-    if (response.error){
-      res.status(response.status).send({
-        status: response.status,
-        error: response.error,
-        message: response.message
-      })
-    }
-
-    res.status(response.status).send({
-      body: response.body,
-    });
+    res.status(response.status).send(response);
   } catch (error) {
     res.status(500).send({
       status: 500,
       error: 'ServerError',
-      message: 'Something went wrong inside the server'
-    })
+      message: 'Something went wrong inside the server',
+    });
   }
 }
 
-module.exports = register
+module.exports = register;
