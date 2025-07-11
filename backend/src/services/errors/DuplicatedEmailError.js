b56d4e3c173f1d6f7ecf6540c2aed152ebf348e7
DavidSheltonSF <david.faria012@gmail.com>
2025-07-10 21:52:56 -0300

    style(DuplicatedEmailError): format with prettier

--- backend/src/services/errors/DuplicatedEmailError.js
+++ backend/src/services/errors/DuplicatedEmailError.js
@@ -1,10 +1,10 @@
-class DuplicatedEmailError extends Error{
-  constructor(email){
+class DuplicatedEmailError extends Error {
+  constructor(email) {
     super(
       `DuplicatedEmailError: the email "${email}" is already associated to a user`
     );
-    this.name = "DuplicatedEmailError";
+    this.name = 'DuplicatedEmailError';
   }
 }
 
-module.exports = DuplicatedEmailError
+module.exports = DuplicatedEmailError;
