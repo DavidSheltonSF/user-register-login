789ef257ca6eca06d43d340a19a934838142d2d4
DavidSheltonSF <david.faria012@gmail.com>
2025-07-12 20:49:48 -0300

    style(validateRequiredFields): remove unnecessary console.logs

--- backend/src/controllers/helpers/validateRequiredFields.js
+++ backend/src/controllers/helpers/validateRequiredFields.js
@@ -3,12 +3,9 @@ const getMissingRequiredFields = require('./getMissingRequiredFields');
 
 function validateRequiredFields(body, requiredFields) {
   const missingRequiredFields = getMissingRequiredFields(body, requiredFields);
-  console.log('validate')
-  console.log(missingRequiredFields);
   if (missingRequiredFields.length > 0) {
     throw new MissingFieldsError(missingRequiredFields);
   }
-
 }
 
 module.exports = validateRequiredFields;
