ab417df99b2ed05303bf210ed80587227f04d2ba
DavidSheltonSF <david.faria012@gmail.com>
2025-07-11 23:23:59 -0300

    fix(register): enhance error handling for DuplicatedEmailError and improve code structure

--- backend/src/routes/register.js
+++ backend/src/routes/register.js
@@ -1,7 +1,12 @@
 const MissingBodyError = require('../controllers/errors/MissingBodyError');
 const MissingFieldsError = require('../controllers/errors/MissingFieldsError');
-const { serverError, badRequest } = require('../controllers/http/http-helpers');
+const {
+  serverError,
+  badRequest,
+  unprocessableEntity,
+} = require('../controllers/http/http-helpers');
 const UserController = require('../controllers/UserController');
+const DuplicatedEmailError = require('../services/errors/DuplicatedEmailError');
 
 async function register(req, res) {
   try {
@@ -10,13 +15,17 @@ async function register(req, res) {
 
     res.status(response.status).send(response);
   } catch (error) {
-    console.log(error)
+    console.log(error);
     if (error instanceof MissingBodyError) {
       return res.status(400).send(badRequest(error.message));
     }
 
     if (error instanceof MissingFieldsError) {
       return res.status(400).send(badRequest(error.message));
+    }
+
+    if (error instanceof DuplicatedEmailError) {
+      return res.status(422).send(unprocessableEntity(error.message));
     }
 
     res.status(500).send(serverError());
